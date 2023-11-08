66bf844a76892d99def2a084d4b1b26d7b2f3a99
JAMES WEI <j65wei@uwaterloo.ca>
2023-11-08 14:25:48 -0500

    test GHA

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_key_pair" "ec2_ssh_keys" {
 }
 
 # Setting up the EC2 instace named as flipp-takehome
-resource "aws_instance" "flipp-takehome-test" {
+resource "aws_instance" "flipp-takehome-test-3" {
   ami             = "ami-089c26792dcb1fbd4"
   instance_type   = var.ec2_type
   key_name        = aws_key_pair.ec2_ssh_keys.key_name
