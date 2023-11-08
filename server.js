3e4043c0c9f2668bf87e3704084ea48c3a1ab763
JAMES WEI <j65wei@uwaterloo.ca>
2023-11-08 01:03:57 -0500

    refactor server.js

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-// Define an endpoint that makes an additional request to ipify and returns whatever that returns
+var public_ip = ""
+
+// Define an endpoint that returns current time, and saved result from ipify
 app.get('/ip', (req, response) => {
-    axios.get('https://api.ipify.org?format=json')
-    .then((res) => {
-        const currentTime = new Date().toLocaleTimeString();
-        response.send(`The host's public ip is ${res.data.ip} and the host time is ${currentTime}`)
-    })
+  const currentTime = new Date().toLocaleTimeString();
+  response.send(`The host's public ip is ${public_ip} and the host time is ${currentTime}`)
 });
 
-// Start the server
+// Start the server, make a request to ipify and save result
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  axios.get('https://api.ipify.org?format=json')
+  .then((res) => {
+    public_ip = res.data.ip
+  })
+  console.log(`Server is running on port ${port} at ${public_ip}, so go to ${public_ip}/api to see host ip and time`);
 });
