dcf3579e9da4e72f6a0d4497d44e5e7868b2042d
JAMES WEI <j65wei@uwaterloo.ca>
2023-11-08 15:35:24 -0500

    add infra config to GHA, use forever in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,13 @@ terraform init
 terraform plan
 terraform apply -auto-approve
 
+echo "[INFO] Waiting for 10 seconds..."
+sleep 10
+
 # Connect to deployed servers using ssh
 # Install Git, nginx, clone repo
 ssh -i flipp-key -o StrictHostKeychecking=no ec2-user@$(terraform output -raw public_ip) "sudo yum install git -y;sudo yum install nginx -y;git clone https://github.com/jameswei1/flipp-takehome.git"
 # Move nginx.conf to correct place, overwrite existing (default) conf, start nginx server
 ssh -i flipp-key -o StrictHostKeychecking=no ec2-user@$(terraform output -raw public_ip) "sudo mv flipp-takehome/nginx.conf /etc/nginx/nginx.conf -f;sudo systemctl start nginx"
-# Install Node, NPM, change directory into flipp-takehome and install dependencies, start Node backend server
-ssh -i flipp-key -o StrictHostKeychecking=no ec2-user@$(terraform output -raw public_ip) "sudo yum install nodejs -y;cd flipp-takehome;npm install;node server.js"
+# Install Node, NPM, forever change directory into flipp-takehome and install dependencies, start Node backend server
+ssh -i flipp-key -o StrictHostKeychecking=no ec2-user@$(terraform output -raw public_ip) "sudo yum install nodejs -y;sudo npm install forever -g;cd flipp-takehome;npm install;forever start server.js"
